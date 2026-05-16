9aba674b42c34c70f586e222ba550325b5e7204c
machidori <Vernusylphgadjet@gmail.com>
2026-05-16 15:08:52 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,141 @@
+# provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "demo-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "demo-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "demo-public-subnet"
+  }
+}
+
+resource "aws_route_table_association" "public_asso" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "sg" {
+  name        = "demo-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo-sg"
+  }
+}
+
+# EC2
+resource "aws_instance" "ec2" {
+  ami                    = "ami-070d2b24928913a49" # Amazon Linux 2023 (東京リージョン例)
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name # セッションマネージャー用IAMロール
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "demo-ec2"
+  }
+}
+
+# Session Manager IAM Role
+resource "aws_iam_role" "ssm_role" {
+  name = "demo-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_fullaccess_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "demo-ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# S3 bucket
+resource "aws_s3_bucket" "s3" {
+  bucket        = "demo-s3-marube23-bucket"
+  force_destroy = true
+}
+
+
+
+output "instance_id" {
+  value = aws_instance.ec2.id
+}
+
+output "s3_bucket" {
+  value = aws_s3_bucket.s3.bucket
+}
